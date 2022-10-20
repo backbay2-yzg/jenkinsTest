64c03e017424f29a9fad16f50c7617f5b17d463b
backbay2 <backbay2@163.com>
2022-10-20 23:08:34 +0800

    Initial commit

---
+++ src/test/java/com/backbay2/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backbay2.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
